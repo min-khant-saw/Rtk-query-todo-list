999a1d7d04d552b18d42dd3f5aa103b57547f7cd
Min Khant Saw <minkhantsaw04@gmail.com>
2022-09-25 10:09:28 +0630

    selector basic

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { deleteData, fetchData, selector } from "./components/store";
+import { useSelector } from "react-redux";
+import { selector } from "./components/store";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const select = useSelector(selector.selectAll);
+  const state = useSelector((state) => state);
   useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
-  return (
-    <div>
-      {select.map((user) => (
-        <span
-          onClick={() =>
-            dispatch(deleteData(user.id)).then((id) =>
-              alert(`this ${id.payload} request id is : "${id.meta.requestId}"`)
-            )
-          }
-          key={user.id}
-          className="d-block"
-        >
-          {user.id} {user.name}
-        </span>
-      ))}
-    </div>
-  );
+    console.log(state);
+  }, []);
+  return <div>{selector(state, state)}</div>;
 };
 
 export default App;
--- src/components/store.js
+++ src/components/store.js
@@ -1,88 +1,25 @@
-import {
-  nanoid,
-  createAsyncThunk,
-  createEntityAdapter,
-  createSlice,
-  combineReducers,
-  configureStore,
-  getDefaultMiddleware,
-} from "@reduxjs/toolkit";
-import axios from "axios";
+import { configureStore, createSelector, createSlice } from "@reduxjs/toolkit";
 
-import logger from "redux-logger";
-
-export const fetchData = createAsyncThunk(
-  "json/user",
-  async (_, { dispatch }) => {
-    return await axios
-      .get(`https://jsonplaceholder.typicode.com/users`)
-      .then((data) => data.data);
-  }
-);
-
-export const deleteData = createAsyncThunk("json/delete-user", async (id) => {
-  axios
-    .get(`https://jsonplaceholder.typicode.com/users/${id}`, {
-      method: "DELETE",
-    })
-    .then((data) => data.data);
-  return id;
-});
-
-const userData = createEntityAdapter({
-  selectId: (user) => user.id,
-});
-
-const userSlice = createSlice({
-  name: "users",
-  initialState: userData.getInitialState({
-    loading: false,
-    status: "offline",
-    error: null,
-  }),
-  reducers: {},
-  extraReducers: {
-    [fetchData.pending]: (state) => {
-      state.loading = true;
-      state.status = "idle";
-    },
-    [fetchData.fulfilled]: (state, { payload }) => {
-      state.loading = false;
-      state.status = "online";
-      userData.setAll(state, payload);
-    },
-    [fetchData.rejected]: (state, action) => {
-      state.loading = false;
-      state.status = "offline";
-      state.error = action.error.message;
-    },
-
-    [deleteData.pending]: (state) => {
-      state.loading = true;
-      state.status = "idle";
-    },
-    [deleteData.fulfilled]: (state, { payload: id }) => {
-      state.loading = false;
-      state.status = "online";
-      userData.removeOne(state, id);
-    },
-    [deleteData.rejected]: (state, action) => {
-      state.loading = false;
-      state.status = "offline";
-      state.error = action.error.message;
-    },
+const slice = createSlice({
+  name: "number",
+  initialState: 0,
+  reducers: {
+    increment: (state) => state + 1,
   },
 });
 
-const combieReduce = combineReducers({
-  users: userSlice.reducer,
-});
+const count = (state) => state;
+const countPlus = (state) => state;
+
+const select = createSelector([count], (c) => c);
+
+export const selector = createSelector(
+  [select, countPlus],
+  (s, num) => s + num + 10
+);
 
 const store = configureStore({
-  reducer: combieReduce,
-  // middleware: [...getDefaultMiddleware(), logger],
+  reducer: slice.reducer,
 });
-
-export const selector = userData.getSelectors((state) => state.users);
 
 export default store;
